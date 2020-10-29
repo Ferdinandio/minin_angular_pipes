ba0b885167bd6498780dae921e8d221ac25b5690
Ferdinandio <dmi8829@yandex.ru>
2020-10-29 22:24:26 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-pipes';
+  title = 'Angular-pipes'
+  search: string = ''
+  searchField = 'title'
+  posts: Post[] = [
+    { title: 'Soda Pop', text: 'Самая лучшая в мире газировка' },
+    { title: 'Bread', text: 'The best bread in the world' },
+    { title: 'Pumpkin', text: 'The most popular pumpkin in the world' }
+  ]
+
+  e: number = Math.E
+  str: string = 'hello world'
+  date = new Date()
+  float = 0.42
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  }
 }
